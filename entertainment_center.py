d23445fe1bb9495c651cd49a536cbb7e967e4540
saneelbhagya <saraswathy.btm@gmail.com>
2017-05-30 16:11:10 -0700

    Added free_tomatoes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,27 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("toy story", "Story of a boy and his toys that come to life", "http://upload.wikimedia.org/wikimedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
-print(toy_story.storyline)
-toy_story.show_trailer()
+#print(toy_story.storyline)
+#toy_story.show_trailer()
+
+avatar = media.Movie("Avatar", "A marine on an alien planet",
+                     "http://upload.wikimedia.org/wikimedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+school_of_rock = media.Movie("school_of_rock", "Storyline",
+                            "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                            "https://www.youtube.com/watch?v=3PsUJFEBC74")
+ratatouille = media.Movie("ratatouille", "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=atLg2wQQxvU")
+hunger_games = media.Movie("Hunger Games", "StoryLine",
+                            "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                            "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,12 +2,12 @@ import webbrowser
 
 class Movie(object):
     """docstring for ."""
-    def __init__(self, title, storyline, poster_image, youtube_trailer):
+    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
-        self.poster_image = poster_image
-        self.youtube_trailer = youtube_trailer
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
 
 
     def show_trailer(self):
-        webbrowser.open(self.youtube_trailer)
+        webbrowser.open(self.trailer_youtube_url)
